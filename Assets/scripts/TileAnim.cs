f01f278dcb810801ef81c9b2409e1eaf02ad7f93
Paul E. Long <paulelong@outlook.com>
2017-12-13 16:56:24 -0500

    Tile animation and script refactoring

---
+++ Assets/scripts/TileAnim.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WordSpell;
+
+public class TileAnim : MonoBehaviour
+{
+    static AudioClip[] AmbientSongs;
+    private System.Random r = new System.Random();
+    public AudioClip SelectSound;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void LoadMusic()
+    {
+        AmbientSongs = Resources.LoadAll<AudioClip>("Songs");
+    }
+
+    public void FinishSpell()
+    {
+        Spells.CastSpell();
+    }
+
+    public void NewSong()
+    {
+        if (AmbientSongs != null)
+        {
+            int rs = r.Next(AmbientSongs.Length);
+            AudioSource asrc = (AudioSource)gameObject.GetComponent(typeof(AudioSource));
+            asrc.clip = AmbientSongs[rs];
+            asrc.PlayDelayed(4);
+        }
+    }
+
+    public void StopSong()
+    {
+        AudioSource asrc = (AudioSource)gameObject.GetComponent(typeof(AudioSource));
+        asrc.Stop();
+    }
+
+    public void PlaySelect()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(SelectSound);
+    }
+
+}
