6d48524e21178f2e78eb485859cc339eb5269979
Paul E. Long <paulelong@outlook.com>
2018-02-07 08:29:43 -0500

    logging refactor

---
+++ Assets/scripts/Engine/Logging.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordSpell
+{
+
+    static class Logging
+    {
+        private static string DebugString = " ";
+        private static string DebugString2 = " ";
+        private static string lastPlayDbg = "";
+
+        public static string StartDbgInfo
+        {
+            get { return DebugString; }
+        }
+
+        public static string PlayDbgInfo
+        {
+            get { return DebugString2; }
+        }
+
+        public static string NewestLog
+        {
+            get
+            {
+                return (DebugString + "\n" + lastPlayDbg);
+            }
+        }
+
+        public static string LastPlayLog
+        {
+            get
+            {
+                return lastPlayDbg;
+            }
+        }
+
+        public static void StartDbg(string s, char sep = ',', bool timestamp = false)
+        {
+            if(timestamp)
+            {
+                DebugString += "\n[" + DateTime.Now.ToString("mm:ss.ff") + "] " + s + "\n";
+            }
+            else
+            {
+                DebugString += s + sep;
+            }
+        }
+
+        public static void PlayDbg(string s, char sep = ',', bool last = false, bool timestamp = false)
+        {
+            if (timestamp)
+            {
+                DebugString2 += "\n[" + DateTime.Now.ToString("mm:ss.ff") + "]" + s + "\n";
+            }
+            else
+            {
+                DebugString2 += s + sep;
+            }
+
+            if (last)
+            {
+                lastPlayDbg = DebugString2;
+                DebugString2 = "";
+            }
+        }
+    }
+}
