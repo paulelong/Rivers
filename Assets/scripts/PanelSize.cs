dbf43aecfa1d1eac4585fef65d567a4dac2042cf
Paul E. Long <paulelong@outlook.com>
2018-03-05 11:18:32 -0500

    Start page adapative sizing

---
+++ Assets/scripts/PanelSize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelSize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        float ratio = (float)Screen.width / (float)Screen.height;
+
+        if(ratio < (.6f))
+        {
+            float newwidth = ratio * 800f;
+
+            gameObject.GetComponent<LayoutElement>().preferredWidth = newwidth;
+        }
+		
+	}
+	
+}
